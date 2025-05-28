e45855984e20fcbf3572ae5c9ef03392af2bce30
Althaf Rizqullah <althaf8747@gmail.com>
2025-05-28 13:59:38 +0700

    edit author

--- pages/3_❓_About_Us.py
+++ pages/3_❓_About_Us.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 
-# CSS untuk menengahkan judul About Us
 center_title_style = """
     <style>
     .center-title {
@@ -16,17 +15,16 @@ center_title_style = """
 st.markdown(center_title_style, unsafe_allow_html=True)
 st.markdown('<h1 class="center-title">About Us</h1>', unsafe_allow_html=True)
 
-def show_author(image_path, name, position, contact):
+def show_author(image_path, name, contact):
     image = Image.open(image_path)
     st.image(image, caption="Foto Author", width=250)
     st.markdown(f"**{name}**")
-    st.markdown(f"Posisi: {position}")
     st.markdown(f"Kontak: [{contact}](mailto:{contact})")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    show_author("images/a.JPG", "Rgsrgs", "Perencana Tambang", "rg.email@example.com")
+    show_author("images/a.JPG", "Althaf Rizqullah", "althafrizq@student.telkomuniversity.ac.id")
 
 with col2:
-    show_author("images/a.JPG", "Nama Kedua", "Posisi Kedua", "email.kedua@example.com")
+    show_author("images/a.JPG", "Evelyn Emery Dahayu", "email.kedua@student.telkomuniversity.ac.id")
